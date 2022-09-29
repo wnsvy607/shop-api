b29d4351575ed58d3372f13c17582b67c6e74dc1
wnsvy607 <wnsvy607@naver.com>
2022-09-30 00:44:32 +0900

    cors 오류 해결

--- src/main/java/com/app/domain/member/constant/Role.java
+++ src/main/java/com/app/domain/member/constant/Role.java
@@ -2,7 +2,7 @@ package com.app.domain.member.constant;
 
 public enum Role {
 
-    USER, ADMIN;
+    USER, EMPLOYEE, ADMIN;
 
     public static Role from(String role) {
         return Role.valueOf(role);
--- src/main/java/com/app/global/config/web/WebSecurityConfig.java
+++ src/main/java/com/app/global/config/web/WebSecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+
+import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -16,11 +21,25 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .cors().and()
                 .authorizeRequests()
                 .antMatchers("api/**").permitAll()
                 .anyRequest().permitAll();
         return http.build();
     }
+
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        final CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(List.of("*"));
+//        configuration.addAllowedHeader("*");
+//        configuration.addAllowedMethod("*");
+//        configuration.setAllowCredentials(true);
+//
+//        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**",configuration);
+//        return source;
+//    }
 
 
 }
